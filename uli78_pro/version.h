c0770279daac632422ec32f87fe86c37da21c1ad
Ulises Freitas <ulises.freitas@gmail.com>
2025-11-25 19:38:32 +0100

    remove  uli78_pro

--- uli78_pro/version.h
+++
@@ -1,7 +0,0 @@
-#define ULI_VERSION_MAJOR       1
-#define ULI_VERSION_MINOR       0
-#define ULI_VERSION_REVISION    0
-#define ULI_VERSION_STATUS      "-dev"
-#define ULI_VERSION_BUILD       ""
-#define ULI_VERSION_YEAR        "2025"
-#define ULI_VERSION_HASH        ""
